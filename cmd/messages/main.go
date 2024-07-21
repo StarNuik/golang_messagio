f64e7c2e4c30bfb977ffdae303c997ebc455fb2c
StarNuik <starnuik@gmail.com>
2024-07-21 03:00:45 +0300

    functions moved out from messages.main into a helper file

--- cmd/messages/main.go
+++ cmd/messages/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -32,18 +30,6 @@ func checkError(err error) {
 	}
 }
 
-func main() {
-	var err error
-	sql, err = internal.NewSqlPool(os.Getenv("SERVICE_POSTGRES_URL"))
-	checkError(err)
-
-	router := gin.Default()
-	router.GET("/healthcheck", healthcheck)
-	router.POST("/message", postMessageRequest)
-
-	router.Run("0.0.0.0:8080")
-}
-
 func healthcheck(c *gin.Context) {
 	c.Status(http.StatusOK)
 }
@@ -71,35 +57,14 @@ func postMessageRequest(c *gin.Context) {
 	c.JSON(http.StatusCreated, msg)
 }
 
-func newMessage(req MessageReq) (Message, error) {
-	if len(req.Content) <= 0 {
-		return Message{}, fmt.Errorf("zero-length content")
-	}
+func main() {
+	var err error
+	sql, err = internal.NewSqlPool(os.Getenv("SERVICE_POSTGRES_URL"))
+	checkError(err)
 
-	len := min(len(req.Content), 4096)
-	id, err := uuid.NewV4()
-	if err != nil {
-		return Message{}, fmt.Errorf("could not generate a uuid")
-	}
+	router := gin.Default()
+	router.GET("/healthcheck", healthcheck)
+	router.POST("/message", postMessageRequest)
 
-	return Message{
-		Id:      id,
-		Created: time.Now().UTC(),
-		Content: req.Content[:len],
-	}, nil
-}
-
-func insertMessage(msg Message) error {
-	tag, err := sql.Exec(
-		context.Background(),
-		"INSERT INTO messages (msg_id, msg_created, msg_content) VALUES ($1, $2, $3)",
-		msg.Id, msg.Created, msg.Content)
-	if err != nil {
-		return err
-	}
-	if tag.RowsAffected() != 1 {
-		//todo: should this be a "stop the service" error or a plain log.error
-		return fmt.Errorf("rowsAffected != 1")
-	}
-	return nil
+	router.Run("0.0.0.0:8080")
 }
